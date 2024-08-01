9d09c99f4002c628ce147685cf5f5cfe92a4df65
Rafael Rarvs <rafael.rarvs@gmail.com>
2024-08-01 01:22:26 -0300

    Finalizado

--- Statistical/estatistica.py
+++
@@ -1,8 +0,0 @@
-class Estatistica:
-    def media(values: list):
-        sum: int = 0;
-        count: int = 0;
-        for item in values :
-            count+1
-            sum += item;
-        return sum/count;
